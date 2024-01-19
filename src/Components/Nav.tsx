988429eb2648dbe77ee1abcd783816e2b94eb9be
gitatractivo <gitanshutalwar@gmail.com>
2024-01-20 03:04:52 +0530

    changes

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,60 +1,33 @@
-import { useState } from "react";
-import {Link} from "react-scroll";
-import {FaTimes} from "react-icons/fa";
-import {CiMenuFries} from "react-icons/ci"
 
-const Nav =()=>{
-    const[click,setClick]=useState(false);
-    const handleClick=()=>{
-        setClick(!click);
-    }
-    const content =
-    <div className="ld:hidden block absolute top-16 w-full left-0 right-0  transition">
-        <ul className="text-center text-xl p-20">
-            
-            <Link spy={true} smooth={true} to="Select Language">
-                <li  className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Select Language</li>
-            </Link>
-            <Link spy={true} smooth={true} to="Login">
-                <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Login</li>
-            </Link>
-            <Link spy={true} smooth={true} to="Sign Up">
-                <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Sign Up</li>
-            </Link>
-        </ul>
-    </div>
-    return(
-       <nav>
-        <div className="h-10vh flex justify-between z-50 text-black lg:py-5 px-20 py-4 flex -1">
-            <div className="flex items-center flex-1">
-                <span className="">LOGO</span>
-                <span className="text-gray-800">BigWigMedia.ai</span>
-            </div>
-            <div className="lg:flex md:flex lg:flex-1 items center justify-end front-normal hidden">
-                <div className="fkex-10">
-                    <ul className="flex gap-8 mr-16 text-[18px]">
-                  
-            <Link spy={true} smooth={true} to="Select Language">
-                <li className="hover:text-fuschsia-600 transition border-b-2 border-slate-900 hover-border-fischia-600 cursor--pointer">Select Language</li>
-            </Link>
-            <Link spy={true} smooth={true} to="Login">
-                <li className="hover:text-fuschsia-600 transition border-b-2 border-slate-900 hover-border-fischia-600 cursor--pointer">Login</li>
-            </Link>
-            <Link spy={true} smooth={true} to="Sign Up">
-                <li className="hover:text-fuschsia-600 bg-black text-white transition border-b-2 border-slate-900 hover-border-fischia-600 cursor--pointer">Sign Up</li>
-            </Link>
-                    </ul>
-                </div>
-            </div>
-            <div>
-                {click &&content}
-            </div>
-            <button className="block sm:hiddden transition" onClick={handleClick}> 
-            </button>
+import { useEffect } from "react";
+import logo from "../assets/Logo.png";
+
+const Nav = () => {
+  
+ 
+  
+  return (
+    <nav>
+      <div className="h-10vh flex justify-between z-50 text-black lg:py-5   px-24 mx-auto py-4  border-b ">
+        <div className="flex items-center gap-4 cursor-pointer">
+          <img src={logo} alt="" />
+          <span className="text-gray-900  font-outfit text-2xl font-semibold">
+            BigWigMedia.ai
+          </span>
         </div>
-
-       </nav>
-    );
+        <div className="flex lg: gap-4 items center justify-end front-normal ">
+          {/* drop down here */}
+          <button className="">Select Language</button>
+          <button className="text-gray-900 font-outfit text-base font-semibold hover:text-gray-700 hover:drop-shadow-2xl ">
+            Login
+          </button>
+          <button className="flex px-4 py-2 justify-center items-center text-white font-outfit text-base font-semibold gap-2 rounded-3xl hover:bg-gray-800 bg-gray-900 shadow-md ">
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
